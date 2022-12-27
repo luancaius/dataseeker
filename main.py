19684c55670f3d3f4ff7ac80917a8a1d4412e7ed
luancaius <luan.ca@outlook.com>
2022-12-26 21:31:03 -0500

    adding simple request code

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
+import sys
+import requests
+def read_request_from_file(filename):
+    return 'https://www.boredapi.com/api/activity?participants=1'
+
+def handle_request(url):
+    response = requests.get(url)
+    return response.json()
+
 if __name__ == "__main__":
     print('starting dataseeker')
+    print(sys.argv)
+
+    filename = sys.argv[1]
+    url = read_request_from_file(filename)
+    response = handle_request(url)
+    print(response)
+
+
+
+
+
+
 
 
