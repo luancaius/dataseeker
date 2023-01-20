257b27f66cc39b544d00d7696b1442080d74a565
luancaius <luan.ca@outlook.com>
2023-01-19 23:57:36 -0500

    refactoring main

--- src/dataseeker_luanramos/entity.py
+++ src/dataseeker_luanramos/entity.py
@@ -5,11 +5,14 @@ class RequestTypeEnum(Enum):
     REST = 0
     SOAP = 1
     GRPC = 2
+    GRAPHQL = 3
 
 
 class RequestEntity:
-    def __init__(self, url: str, request_type: RequestTypeEnum, headers: dict, body: str):
+    def __init__(self, url: str, request_type: RequestTypeEnum, operation: str,  headers: dict, body: str):
         self.url = url
         self.request_type = request_type
+        self.operation = operation
         self.header = headers
         self.body = body
+
--- src/dataseeker_luanramos/main.py
+++ src/dataseeker_luanramos/main.py
@@ -1,6 +1,30 @@
+import requests
+from src.dataseeker_luanramos.entity import RequestEntity, RequestTypeEnum
 
-def show_value(value):
-    print(value)
 
-def sum(a,b):
-    return a+b
+class DataSeeker:
+    @staticmethod
+    def create_request(url: str, metadata: dict):
+        request = None
+        if not url:
+            raise Exception("Cant create a request without URL")
+        if metadata is None:
+            request = RequestEntity(url, RequestTypeEnum.REST, None, None)
+        if metadata['type_request'] == 'REST':
+            type_request = RequestTypeEnum.REST
+            operation = metadata['operation']
+            request = RequestEntity(url, type_request, operation, None)
+        return request
+
+    def __init__(self, request: RequestEntity):
+        self.request = request
+
+    def handle(self):
+        response = None
+        if self.request.type_request == RequestTypeEnum.REST:
+            if self.request.operation == 'GET':
+                response = requests.get(self.request.url, headers=self.request.headers)
+        return response
+
+
+
